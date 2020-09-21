afe8369dbf735b42f916dccc1eb05942206742a2
Kasper Nurminen <kasper.nurminen@vincit.fi>
2020-09-21 16:19:19 +0300

    Updated static files

--- js/compiled/release/clojure_cv/core.js
+++ js/compiled/release/clojure_cv/core.js
@@ -31,104 +31,152 @@ clojure_cv.core.mount_root = (function clojure_cv$core$mount_root(){
 
 return reagent.core.render.cljs$core$IFn$_invoke$arity$2(new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [clojure_cv.views.core.main_panel,clojure_cv.core.page], null),document.getElementById("app"));
 });
-var action__9629__auto___9759 = (function (params__9630__auto__){
-if(cljs.core.map_QMARK_(params__9630__auto__)){
-var map__9754 = params__9630__auto__;
-var map__9754__$1 = ((((!((map__9754 == null)))?(((((map__9754.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9754.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9754):map__9754);
+var action__9632__auto___9762 = (function (params__9633__auto__){
+if(cljs.core.map_QMARK_(params__9633__auto__)){
+var map__9757 = params__9633__auto__;
+var map__9757__$1 = ((((!((map__9757 == null)))?(((((map__9757.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9757.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9757):map__9757);
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.core.main_page);
 } else {
-if(cljs.core.vector_QMARK_(params__9630__auto__)){
-var vec__9756 = params__9630__auto__;
+if(cljs.core.vector_QMARK_(params__9633__auto__)){
+var vec__9759 = params__9633__auto__;
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.core.main_page);
 } else {
 return null;
 }
 }
 });
-secretary.core.add_route_BANG_("/",action__9629__auto___9759);
+secretary.core.add_route_BANG_("/",action__9632__auto___9762);
 
-var action__9629__auto___9765 = (function (params__9630__auto__){
-if(cljs.core.map_QMARK_(params__9630__auto__)){
-var map__9760 = params__9630__auto__;
-var map__9760__$1 = ((((!((map__9760 == null)))?(((((map__9760.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9760.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9760):map__9760);
+var action__9632__auto___9768 = (function (params__9633__auto__){
+if(cljs.core.map_QMARK_(params__9633__auto__)){
+var map__9763 = params__9633__auto__;
+var map__9763__$1 = ((((!((map__9763 == null)))?(((((map__9763.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9763.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9763):map__9763);
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.ilmomasiina);
 } else {
-if(cljs.core.vector_QMARK_(params__9630__auto__)){
-var vec__9762 = params__9630__auto__;
+if(cljs.core.vector_QMARK_(params__9633__auto__)){
+var vec__9765 = params__9633__auto__;
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.ilmomasiina);
 } else {
 return null;
 }
 }
 });
-secretary.core.add_route_BANG_("/portfolio/ilmomasiina",action__9629__auto___9765);
+secretary.core.add_route_BANG_("/portfolio/ilmomasiina",action__9632__auto___9768);
 
-var action__9629__auto___9771 = (function (params__9630__auto__){
-if(cljs.core.map_QMARK_(params__9630__auto__)){
-var map__9766 = params__9630__auto__;
-var map__9766__$1 = ((((!((map__9766 == null)))?(((((map__9766.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9766.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9766):map__9766);
+var action__9632__auto___9774 = (function (params__9633__auto__){
+if(cljs.core.map_QMARK_(params__9633__auto__)){
+var map__9769 = params__9633__auto__;
+var map__9769__$1 = ((((!((map__9769 == null)))?(((((map__9769.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9769.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9769):map__9769);
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.kaspernurminen);
 } else {
-if(cljs.core.vector_QMARK_(params__9630__auto__)){
-var vec__9768 = params__9630__auto__;
+if(cljs.core.vector_QMARK_(params__9633__auto__)){
+var vec__9771 = params__9633__auto__;
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.kaspernurminen);
 } else {
 return null;
 }
 }
 });
-secretary.core.add_route_BANG_("/portfolio/kaspernurminen",action__9629__auto___9771);
+secretary.core.add_route_BANG_("/portfolio/kaspernurminen",action__9632__auto___9774);
 
-var action__9629__auto___9777 = (function (params__9630__auto__){
-if(cljs.core.map_QMARK_(params__9630__auto__)){
-var map__9772 = params__9630__auto__;
-var map__9772__$1 = ((((!((map__9772 == null)))?(((((map__9772.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9772.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9772):map__9772);
+var action__9632__auto___9780 = (function (params__9633__auto__){
+if(cljs.core.map_QMARK_(params__9633__auto__)){
+var map__9775 = params__9633__auto__;
+var map__9775__$1 = ((((!((map__9775 == null)))?(((((map__9775.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9775.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9775):map__9775);
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.oloscreen);
 } else {
-if(cljs.core.vector_QMARK_(params__9630__auto__)){
-var vec__9774 = params__9630__auto__;
+if(cljs.core.vector_QMARK_(params__9633__auto__)){
+var vec__9777 = params__9633__auto__;
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.oloscreen);
 } else {
 return null;
 }
 }
 });
-secretary.core.add_route_BANG_("/portfolio/oloscreen",action__9629__auto___9777);
+secretary.core.add_route_BANG_("/portfolio/oloscreen",action__9632__auto___9780);
 
-var action__9629__auto___9783 = (function (params__9630__auto__){
-if(cljs.core.map_QMARK_(params__9630__auto__)){
-var map__9778 = params__9630__auto__;
-var map__9778__$1 = ((((!((map__9778 == null)))?(((((map__9778.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9778.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9778):map__9778);
+var action__9632__auto___9786 = (function (params__9633__auto__){
+if(cljs.core.map_QMARK_(params__9633__auto__)){
+var map__9781 = params__9633__auto__;
+var map__9781__$1 = ((((!((map__9781 == null)))?(((((map__9781.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9781.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9781):map__9781);
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.infodisplay);
 } else {
-if(cljs.core.vector_QMARK_(params__9630__auto__)){
-var vec__9780 = params__9630__auto__;
+if(cljs.core.vector_QMARK_(params__9633__auto__)){
+var vec__9783 = params__9633__auto__;
 return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.infodisplay);
 } else {
 return null;
 }
 }
 });
-secretary.core.add_route_BANG_("/portfolio/infodisplay",action__9629__auto___9783);
+secretary.core.add_route_BANG_("/portfolio/infodisplay",action__9632__auto___9786);
+
+var action__9632__auto___9792 = (function (params__9633__auto__){
+if(cljs.core.map_QMARK_(params__9633__auto__)){
+var map__9787 = params__9633__auto__;
+var map__9787__$1 = ((((!((map__9787 == null)))?(((((map__9787.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9787.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9787):map__9787);
+return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.asumistukilaskuri);
+} else {
+if(cljs.core.vector_QMARK_(params__9633__auto__)){
+var vec__9789 = params__9633__auto__;
+return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.asumistukilaskuri);
+} else {
+return null;
+}
+}
+});
+secretary.core.add_route_BANG_("/portfolio/asumistukilaskuri",action__9632__auto___9792);
+
+var action__9632__auto___9798 = (function (params__9633__auto__){
+if(cljs.core.map_QMARK_(params__9633__auto__)){
+var map__9793 = params__9633__auto__;
+var map__9793__$1 = ((((!((map__9793 == null)))?(((((map__9793.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9793.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9793):map__9793);
+return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.gameservice);
+} else {
+if(cljs.core.vector_QMARK_(params__9633__auto__)){
+var vec__9795 = params__9633__auto__;
+return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.gameservice);
+} else {
+return null;
+}
+}
+});
+secretary.core.add_route_BANG_("/portfolio/gameservice",action__9632__auto___9798);
+
+var action__9632__auto___9804 = (function (params__9633__auto__){
+if(cljs.core.map_QMARK_(params__9633__auto__)){
+var map__9799 = params__9633__auto__;
+var map__9799__$1 = ((((!((map__9799 == null)))?(((((map__9799.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__9799.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__9799):map__9799);
+return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.ydinbook);
+} else {
+if(cljs.core.vector_QMARK_(params__9633__auto__)){
+var vec__9801 = params__9633__auto__;
+return cljs.core.reset_BANG_(clojure_cv.core.selected_page,clojure_cv.views.portfolio.ydinbook);
+} else {
+return null;
+}
+}
+});
+secretary.core.add_route_BANG_("/portfolio/ydinbook",action__9632__auto___9804);
 
 clojure_cv.core.hook_browser_navigation_BANG_ = (function clojure_cv$core$hook_browser_navigation_BANG_(){
-var G__9784 = (new goog.History());
-var G__9785_9788 = G__9784;
-var G__9786_9789 = goog.history.EventType.NAVIGATE;
-var G__9787_9790 = ((function (G__9785_9788,G__9786_9789,G__9784){
+var G__9805 = (new goog.History());
+var G__9806_9809 = G__9805;
+var G__9807_9810 = goog.history.EventType.NAVIGATE;
+var G__9808_9811 = ((function (G__9806_9809,G__9807_9810,G__9805){
 return (function (event){
 return secretary.core.dispatch_BANG_(event.token);
-});})(G__9785_9788,G__9786_9789,G__9784))
+});})(G__9806_9809,G__9807_9810,G__9805))
 ;
-goog.events.listen(G__9785_9788,G__9786_9789,G__9787_9790);
+goog.events.listen(G__9806_9809,G__9807_9810,G__9808_9811);
 
-G__9784.setEnabled(true);
+G__9805.setEnabled(true);
 
-return G__9784;
+return G__9805;
 });
 clojure_cv.core.init = (function clojure_cv$core$init(){
-var G__9791_9792 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$clojure_DASH_cv$events_SLASH_initialize_DASH_db], null);
-(re_frame.core.dispatch_sync.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.dispatch_sync.cljs$core$IFn$_invoke$arity$1(G__9791_9792) : re_frame.core.dispatch_sync.call(null,G__9791_9792));
+var G__9812_9813 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$clojure_DASH_cv$events_SLASH_initialize_DASH_db], null);
+(re_frame.core.dispatch_sync.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.dispatch_sync.cljs$core$IFn$_invoke$arity$1(G__9812_9813) : re_frame.core.dispatch_sync.call(null,G__9812_9813));
 
 accountant.core.configure_navigation_BANG_(new cljs.core.PersistentArrayMap(null, 2, [cljs.core.cst$kw$nav_DASH_handler,(function (path){
 return secretary.core.dispatch_BANG_(path);
--- js/compiled/release/clojure_cv/views/portfolio.js
+++ js/compiled/release/clojure_cv/views/portfolio.js
@@ -14,12 +14,12 @@ return cljs.core.with_meta(new cljs.core.PersistentVector(null, 2, 5, cljs.core.
 }),buttons)], null);
 });
 clojure_cv.views.portfolio.render_portfolio_section = (function clojure_cv$views$portfolio$render_portfolio_section(key){
-var with_let7648 = reagent.ratom.with_let_values(cljs.core.cst$kw$with_DASH_let7648);
-var temp__4661__auto___7653 = reagent.ratom._STAR_ratom_context_STAR_;
-if((temp__4661__auto___7653 == null)){
+var with_let7651 = reagent.ratom.with_let_values(cljs.core.cst$kw$with_DASH_let7651);
+var temp__4661__auto___7656 = reagent.ratom._STAR_ratom_context_STAR_;
+if((temp__4661__auto___7656 == null)){
 } else {
-var c__2555__auto___7654 = temp__4661__auto___7653;
-if((with_let7648.generation === c__2555__auto___7654.ratomGeneration)){
+var c__2555__auto___7657 = temp__4661__auto___7656;
+if((with_let7651.generation === c__2555__auto___7657.ratomGeneration)){
 if(cljs.core.truth_(reagent.debug.has_console)){
 (cljs.core.truth_(reagent.debug.tracking)?reagent.debug.track_console:console).error(["Warning: The same with-let is being used more ","than once in the same reactive context."].join(''));
 } else {
@@ -27,22 +27,22 @@ if(cljs.core.truth_(reagent.debug.has_console)){
 } else {
 }
 
-with_let7648.generation = c__2555__auto___7654.ratomGeneration;
+with_let7651.generation = c__2555__auto___7657.ratomGeneration;
 }
 
 
-var init7649 = (with_let7648.length === (0));
-var _ = ((init7649)?(with_let7648[(0)] = (function (){var G__7650 = new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$set_DASH_current_DASH_section,cljs.core.name(key)], null);
-return (re_frame.core.dispatch.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.dispatch.cljs$core$IFn$_invoke$arity$1(G__7650) : re_frame.core.dispatch.call(null,G__7650));
-})()):(with_let7648[(0)]));
-var res__2556__auto__ = (function (){var image_enlarged = (function (){var G__7651 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$image_DASH_enlarged], null);
-return (re_frame.core.subscribe.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.subscribe.cljs$core$IFn$_invoke$arity$1(G__7651) : re_frame.core.subscribe.call(null,G__7651));
+var init7652 = (with_let7651.length === (0));
+var _ = ((init7652)?(with_let7651[(0)] = (function (){var G__7653 = new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$set_DASH_current_DASH_section,cljs.core.name(key)], null);
+return (re_frame.core.dispatch.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.dispatch.cljs$core$IFn$_invoke$arity$1(G__7653) : re_frame.core.dispatch.call(null,G__7653));
+})()):(with_let7651[(0)]));
+var res__2556__auto__ = (function (){var image_enlarged = (function (){var G__7654 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$image_DASH_enlarged], null);
+return (re_frame.core.subscribe.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.subscribe.cljs$core$IFn$_invoke$arity$1(G__7654) : re_frame.core.subscribe.call(null,G__7654));
 })();
-return new cljs.core.PersistentVector(null, 6, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$section,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$id,key], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$row$align_DASH_items_DASH_center,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$col_DASH_md_DASH_7,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$h1$text_DASH_center,cljs.core.get_in.cljs$core$IFn$_invoke$arity$2(clojure_cv.texts.texts,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [key,cljs.core.cst$kw$title], null))], null)], null),new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$col_DASH_md_DASH_5,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$style,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$font_DASH_family,"monospace"], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$p,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$i,"Used technologies:"], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$p,cljs.core.get_in.cljs$core$IFn$_invoke$arity$2(clojure_cv.texts.texts,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [key,cljs.core.cst$kw$technologies], null))], null)], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$row,(cljs.core.truth_(cljs.core.deref(image_enlarged))?new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$cover], null):null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$portfolio_DASH_image$detail_DASH_page,cljs.core.merge.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2([(cljs.core.truth_(cljs.core.deref(image_enlarged))?new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$class,"enlarged"], null):null),new cljs.core.PersistentArrayMap(null, 2, [cljs.core.cst$kw$on_DASH_click,((function (image_enlarged,init7649,_,with_let7648){
+return new cljs.core.PersistentVector(null, 6, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$section,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$id,key], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$row$align_DASH_items_DASH_center,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$col_DASH_md_DASH_7,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$h1$text_DASH_center,cljs.core.get_in.cljs$core$IFn$_invoke$arity$2(clojure_cv.texts.texts,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [key,cljs.core.cst$kw$title], null))], null)], null),new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$col_DASH_md_DASH_5,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$style,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$font_DASH_family,"monospace"], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$p,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$i,"Used technologies:"], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$p,cljs.core.get_in.cljs$core$IFn$_invoke$arity$2(clojure_cv.texts.texts,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [key,cljs.core.cst$kw$technologies], null))], null)], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$row,(cljs.core.truth_(cljs.core.deref(image_enlarged))?new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$cover], null):null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$portfolio_DASH_image$detail_DASH_page,cljs.core.merge.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2([(cljs.core.truth_(cljs.core.deref(image_enlarged))?new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$class,"enlarged"], null):null),new cljs.core.PersistentArrayMap(null, 2, [cljs.core.cst$kw$on_DASH_click,((function (image_enlarged,init7652,_,with_let7651){
 return (function (){
-var G__7652 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$toggle_DASH_image_DASH_enlargement], null);
-return (re_frame.core.dispatch.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.dispatch.cljs$core$IFn$_invoke$arity$1(G__7652) : re_frame.core.dispatch.call(null,G__7652));
-});})(image_enlarged,init7649,_,with_let7648))
+var G__7655 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$toggle_DASH_image_DASH_enlargement], null);
+return (re_frame.core.dispatch.cljs$core$IFn$_invoke$arity$1 ? re_frame.core.dispatch.cljs$core$IFn$_invoke$arity$1(G__7655) : re_frame.core.dispatch.call(null,G__7655));
+});})(image_enlarged,init7652,_,with_let7651))
 ,cljs.core.cst$kw$style,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$background_DASH_image,["url(../../assets/",cljs.core.str.cljs$core$IFn$_invoke$arity$1(cljs.core.name(key)),".webp)"].join('')], null)], null)], 0))], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$row$m_DASH_4,cljs.core.get_in.cljs$core$IFn$_invoke$arity$2(clojure_cv.texts.texts,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [key,cljs.core.cst$kw$text], null))], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$row$flex_DASH_column,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [clojure_cv.views.portfolio.render_buttons,cljs.core.get_in.cljs$core$IFn$_invoke$arity$2(clojure_cv.texts.texts,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [key,cljs.core.cst$kw$buttons], null))], null)], null)], null);
 })();
 
@@ -59,4 +59,13 @@ return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMP
 });
 clojure_cv.views.portfolio.infodisplay = (function clojure_cv$views$portfolio$infodisplay(){
 return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [clojure_cv.views.portfolio.render_portfolio_section,cljs.core.cst$kw$infodisplay], null);
+});
+clojure_cv.views.portfolio.gameservice = (function clojure_cv$views$portfolio$gameservice(){
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [clojure_cv.views.portfolio.render_portfolio_section,cljs.core.cst$kw$gameservice], null);
+});
+clojure_cv.views.portfolio.asumistukilaskuri = (function clojure_cv$views$portfolio$asumistukilaskuri(){
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [clojure_cv.views.portfolio.render_portfolio_section,cljs.core.cst$kw$asumistukilaskuri], null);
+});
+clojure_cv.views.portfolio.ydinbook = (function clojure_cv$views$portfolio$ydinbook(){
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [clojure_cv.views.portfolio.render_portfolio_section,cljs.core.cst$kw$ydinbook], null);
 });
